481c71c74fe047e49dd146f358644ca9e2530ec6
Ojas Singh <ojas.singh02@gmail.com>
2025-07-09 18:16:59 -0400

    Minor updates

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,33 +1,12 @@
 "use client";
-import React, { useEffect } from "react";
+import React from "react";
 import { FC } from "react";
 import BlurFade from "./ui/blur-fade";
 import { Plus, Sparkles } from "lucide-react";
 import Nav from "./nav";
 
 const Hero: FC = () => {
-  useEffect(() => {
-    const handleKeyPress = (event: KeyboardEvent) => {
-      const key = event.key.toLowerCase();
-
-      switch (key) {
-        case "l":
-          window.open("https://www.linkedin.com/in/ojas-singh/", "_blank");
-          break;
-        case "g":
-          window.open("https://github.com/ojassingh", "_blank");
-          break;
-        case "w":
-          document
-            .querySelector("#work")
-            ?.scrollIntoView({ behavior: "smooth" });
-          break;
-      }
-    };
-
-    window.addEventListener("keydown", handleKeyPress);
-    return () => window.removeEventListener("keydown", handleKeyPress);
-  }, []);
+ 
 
   return (
     <>
@@ -37,8 +16,8 @@ const Hero: FC = () => {
       >
         <Nav />
 
-        <div className="row-start-5">
-          <div className="">
+        <div className="row-start-5 z-20">
+          <div className="z-20">
             <BlurFade inView delay={0.1}>
               <Sparkles fill="white" className="size-10" />
             </BlurFade>
@@ -48,16 +27,17 @@ const Hero: FC = () => {
               </h2>
             </BlurFade>
             <BlurFade inView delay={0.3}>
-              <h1 className="min-w-max text-left font-serif text-7xl font-medium text-white sm:text-9xl">
+              <h1 className="min-w-max text-left text-7xl font-medium text-white sm:text-9xl">
                 ojas singh
               </h1>
             </BlurFade>
           </div>
         </div>
 
-        <div className="col-span-1 row-start-11 z-10">
+        <div className="col-span-2 row-start-11 z-30">
           <p className="text-left text-base text-gray-400">
-            I&apos;m a developer based in Toronto. <br/> Studied Physics and Statistics at the University of Toronto.
+            I&apos;m a software engineer based in Toronto. <br/> Studied Physics and Statistics at the UofT.
+            <br/>
             I&apos;m passionate about design engineering and AI to augment our
             current capabilities.
             <br />
@@ -70,7 +50,7 @@ const Hero: FC = () => {
         <div className="absolute right-0 bottom-0 translate-x-[50%] translate-y-[50%]">
           <Plus className="size-3 text-neutral-400" />
         </div>
-        <div className="absolute bottom-0 z-0 h-40 w-full bg-line ar-to-b from-transparent to-black"></div>
+        <div className="absolute bottom-0 z-20 h-40 w-full bg-linear-to-b from-transparent to-black"></div>
       </section>
       
       <video
@@ -79,7 +59,7 @@ const Hero: FC = () => {
         autoPlay
         muted
         playsInline
-        className="absolute inset-0 -z-10 mx-auto h-screen w-full object-cover opacity-40 sm:max-w-7xl"
+        className="absolute inset-0 mx-auto h-screen w-full object-cover opacity-30 sm:max-w-7xl"
       ></video>
     </>
   );
--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,11 +1,37 @@
-import { ArrowUpRight } from "lucide-react";
+import { ArrowUpRight, Copy } from "lucide-react";
 import Link from "next/link";
 import BlurFade from "./ui/blur-fade";
-import { EmailButton } from "./ui/email-button";
+import { useEffect } from "react";
+import { toast } from "sonner";
 
 export default function Nav() {
+  useEffect(() => {
+    const handleKeyPress = (event: KeyboardEvent) => {
+      const key = event.key.toLowerCase();
+
+      switch (key) {
+        case "l":
+          window.open("https://www.linkedin.com/in/ojas-singh/", "_blank");
+          break;
+        case "g":
+          window.open("https://github.com/ojassingh", "_blank");
+          break;
+        case "x":
+          window.open("https://x.com/ojassinghh", "_blank");
+          break;
+        case "e":
+          navigator.clipboard.writeText("ojas.singh@gmail.com");
+          toast.success("Email Copied!");
+          break;
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyPress);
+    return () => window.removeEventListener("keydown", handleKeyPress);
+  }, []);
+
   return (
-    <div className="row-start-3 flex place-content-between pt-10 sm:col-span-6 sm:col-start-1 sm:row-start-2 sm:place-content-start sm:gap-1 sm:pt-0">
+    <div className="row-start-3 z-20 flex place-content-between pt-10 sm:col-span-6 sm:col-start-1 sm:row-start-2 sm:place-content-start sm:gap-1 sm:pt-0">
       <BlurFade delay={0.4} className="">
         <Link
           target="_blank"
@@ -30,7 +56,22 @@ export default function Nav() {
         </Link>
       </BlurFade>
       <BlurFade delay={0.4 + 0.1}>
-        <EmailButton />
+        <div className="group flex items-center">
+          <p>[E] email</p>
+          <span className="block sm:hidden">[email]</span>
+          <Copy className="mr-1 hidden h-4 w-4 rotate-45 opacity-0 duration-300 group-hover:ml-1 group-hover:rotate-0 group-hover:opacity-100 sm:block" />
+        </div>
+      </BlurFade>
+      <BlurFade delay={0.4 + 0.15}>
+        <Link
+          target="_blank"
+          href="https://x.com/ojassinghh"
+          className="group flex items-center"
+        >
+          <p>[X] X</p>
+          <span className="block sm:hidden">[x]</span>
+          <ArrowUpRight className="mr-1 hidden h-4 w-4 rotate-45 opacity-0 duration-300 group-hover:ml-1 group-hover:rotate-0 group-hover:opacity-100 sm:block" />
+        </Link>
       </BlurFade>
     </div>
   );
