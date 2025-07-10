39ae8201be25539a46326473ca5d0a2c1a08faa5
Ojas Singh <ojas.singh02@gmail.com>
2025-07-09 23:39:41 -0400

    Clean footer gradient in tailwind

---
+++ src/components/ui/footer-gradient.tsx
@@ -0,0 +1,8 @@
+
+
+export function FooterGradient() {
+  return (
+    <div className="h-14 fixed bottom-0 w-full inset-x-0 pointer-events-none backdrop-blur-md bg-gradient-to-t from-black/80 via-black/40 to-transparent mask-t-from-0% mask-t-to-100%">
+    </div>
+  )
+}
