409ead25737f0047236ccf9e8cbdc5f395d7bb2b
Ojas Singh <ojas.singh02@gmail.com>
2025-07-10 21:38:31 -0400

    Fix footer gradient to be more sutble

--- src/components/ui/footer-gradient.tsx
+++ src/components/ui/footer-gradient.tsx
@@ -2,7 +2,7 @@
 
 export function FooterGradient() {
   return (
-    <div className="h-14 fixed bottom-0 w-full inset-x-0 pointer-events-none backdrop-blur-md bg-gradient-to-t from-black/80 via-black/40 to-transparent mask-t-from-0% mask-t-to-100%">
+    <div className="h-10 fixed bottom-0 w-full inset-x-0 pointer-events-none backdrop-blur-lg bg-gradient-to-t from-black/50 via-black/50 to-transparent mask-t-from-0% mask-t-to-100%">
     </div>
   )
 }
