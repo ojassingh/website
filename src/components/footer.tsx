2f1fed406bad2a0b57876dd912c4bb6366698ecb
Ojas Singh <ojas.singh02@gmail.com>
2025-07-10 22:42:18 -0400

    Update footer video

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,12 +5,12 @@ const Footer = () => {
     <footer className="relative text-center">
       <div className="absolute inset-x-0 top-0 z-10 h-40 bg-linear-to-b from-black to-transparent"></div>
       <video
-        src="/warp-vid.webm"
+        src="/footer-1.webm"
         loop
         autoPlay
         muted
         playsInline
-        className="absolute w-7xl object-cover opacity-40"
+        className=""
       ></video>
       <div className="absolute inset-x-0 bottom-0 h-40 bg-linear-to-b from-transparent to-black"></div>
     </footer>
