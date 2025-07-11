2f4439d528e0e8d0424c1deac3fa80b254f83ed4
Ojas Singh <ojas.singh02@gmail.com>
2025-07-11 16:27:06 -0400

    Clean up

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Page() {
+    return (
+        <main className="max-w-7xl mx-auto p-20">
+            <div className="">
+                <h1>Will be posting some thoughts here soon.</h1>
+            </div>
+        </main>
+    )
+}
