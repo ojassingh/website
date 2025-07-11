d7149ed40c193c6e2c76acf4f3ed6978723a4770
Ojas Singh <ojas.singh02@gmail.com>
2025-07-11 18:36:59 -0400

    Fix mdx layout

--- src/app/work/[slug]/page.tsx
+++ src/app/work/[slug]/page.tsx
@@ -48,7 +48,7 @@ export default async function Page(props: {
         <title className="">{frontmatter.title} | Ojas Singh</title>
         <meta name="description" content={frontmatter.description} />
       </Head>
-      <div className="mx-auto max-w-2xl pt-6">
+      <div className="mx-auto max-w-3xl p-6">
         <div className="flex items-center justify-between">
           <Link
             href="/"
@@ -65,7 +65,7 @@ export default async function Page(props: {
             </Link>
           </div>
         </div>
-        <div className="py-14">
+        <div className="py-14 grid place-content-center">
           <article className="">
             <div className="tracking-tighter">
               <h1 className="text-center text-2xl font-medium">
@@ -76,17 +76,36 @@ export default async function Page(props: {
               </p>
             </div>
             <div className="relative mt-8">
-              <div className="absolute top-1/2 left-1/2 -z-10 h-[150%] w-[105%] -translate-x-1/2 -translate-y-1/2 bg-radial-[at_50%_50%] from-purple-800 to-transparent to-65%"></div>
+              <div 
+                className="absolute top-1/2 left-1/2 -z-10 h-[150%] w-[105%] -translate-x-1/2 -translate-y-1/2"
+                style={{
+                  background: `radial-gradient(at 50% 50%, ${frontmatter.color} 0%, transparent 65%)`
+                }}
+              ></div>
               <Image
                 alt={frontmatter.title + ` ${frontmatter.description}`}
                 src={frontmatter.thumbnail}
                 width={1000}
                 height={1000}
-                className="h-[20rem] w-full rounded-lg object-cover object-center shadow-md"
+                className="h-full w-3xl rounded-lg object-cover object-center shadow-md"
               />
             </div>
           </article>
-          <div className="prose dark:prose-invert prose-headings:font-medium prose-headings:tracking-tighter prose-h1:text-2xl prose-h2:text-xl prose-h3:text-lg mt-14">
+          <div className="grid grid-cols-3 items-start gap-6 mt-14">
+            <div className="col-span-1 flex flex-col gap-2">
+              <h4 className="text-lg font-medium">Stack</h4>
+              <ul className="text-neutral-300 tracking-tighter">
+              {frontmatter.stack.map((stack: string, index: number) => (
+                <li key={index}>{stack}</li>
+              ))}
+            </ul>
+            </div>
+            <div className="col-span-2">
+              <p className="text-lg font-medium">TLDR</p>
+            <p className="text-neutral-300 tracking-tighter whitespace-pre-line">{frontmatter.tldr}</p>
+            </div>
+          </div>
+          <div className="mt-14">
             <Post />
           </div>
         </div>
