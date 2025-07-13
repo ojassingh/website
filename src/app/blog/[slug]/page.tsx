fe57ab42d94f96f2a3a270b6f19a305b7ee88ffb
Ojas Singh <ojas.singh02@gmail.com>
2025-07-12 22:31:03 -0400

    Update blog page, get rid of bad layout

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import type { Metadata } from "next";
-import { Dot } from "lucide-react";
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 import Head from "next/head";
+import { TableOfContents } from "@/components/ui/table-of-contents";
+import { extractHeadings } from "@/lib/headings";
+import Link from "next/link";
+import { ArrowLeft } from "lucide-react";
 
 export async function generateStaticParams() {
   const postsDir = path.join(process.cwd(), "src/content");
@@ -18,18 +21,13 @@ export async function generateMetadata(props: {
   params: Promise<{ slug: string }>;
 }): Promise<Metadata> {
   const params = await props.params;
-  const filePath = path.join(
-    process.cwd(),
-    "src/content",
-    `${params.slug}.mdx`,
-  );
+  const filePath = path.join(process.cwd(), "src/content", `${params.slug}.mdx`);
   const fileContent = fs.readFileSync(filePath, "utf-8");
   const { data: frontmatter } = matter(fileContent);
 
   return {
     title: frontmatter.title || "Untitled",
-    description:
-      frontmatter.description || "A compliance law company based in India",
+    description: frontmatter.description || "Ojas Singh",
   };
 }
 
@@ -37,54 +35,63 @@ export default async function Page(props: {
   params: Promise<{ slug: string }>;
 }) {
   const params = await props.params;
-  const filePath = path.join(
-    process.cwd(),
-    "src/content",
-    `${params.slug}.mdx`,
-  );
+  const filePath = path.join(process.cwd(), "src/content", `${params.slug}.mdx`);
   const fileContent = fs.readFileSync(filePath, "utf-8");
-  const { data: frontmatter } = matter(fileContent);
+  const { data: frontmatter, content } = matter(fileContent);
+  const headings = extractHeadings(content);
 
   const { default: Post } = await import(`@/content/${params.slug}.mdx`);
 
   return (
-    <div className="">
+    <>
       <Head>
-        <title>{frontmatter.title} | Vandana Singh & Associates</title>
+        <title className="">{frontmatter.title} | Ojas Singh</title>
         <meta name="description" content={frontmatter.description} />
       </Head>
-      <article className="grid gap-6">
-        <ul className="flex gap-2">
-          <p>{frontmatter.date}</p>
-          <Dot />
-          <p>5 min read</p>
-        </ul>
-        <h1 className="font-heading text-3xl sm:text-5xl">
-          {frontmatter.title}
-        </h1>
-        <div className="flex items-center gap-2">
-          <Image
-            alt={frontmatter.author}
-            src={frontmatter.headshot}
-            width={40}
-            height={40}
-            className="h-10 w-10 rounded-full object-cover shadow-md"
-          />
-          <div className="grid gap-0">
-            <p>{frontmatter.author}</p>
-            <span className="text-sm text-gray-500">{frontmatter.role}</span>
+      <div className="mx-auto max-w-3xl p-6">
+        <div className="flex items-center justify-between">
+          <Link
+            href="/"
+            className="flex items-center gap-2 text-neutral-400 tracking-tight"
+          >
+            <ArrowLeft className="size-4" /> Back
+          </Link>
+          <p className="text-neutral-400">{frontmatter.date}</p>
+        </div>
+        <div className="py-14 grid place-content-center">
+          <article className="">
+            <div className="tracking-tighter">
+              <h1 className="text-center text-2xl font-medium">
+                {frontmatter.title}
+              </h1>
+            </div>
+            <div className="relative mt-8">
+              <div 
+                className="absolute top-1/2 left-1/2 -z-10 h-[150%] w-[105%] -translate-x-1/2 -translate-y-1/2"
+                style={{
+                  background: `radial-gradient(at 50% 50%, ${frontmatter.color} 0%, transparent 65%)`
+                }}
+              ></div>
+              <Image
+                alt={frontmatter.title + ` ${frontmatter.description}`}
+                src={frontmatter.thumbnail}
+                width={1000}
+                height={1000}
+                className="h-full w-3xl rounded-lg object-cover object-center shadow-md"
+              />
+            </div>
+          </article>
+          <div className="mt-14">
+            <h4 className="text-xl font-medium">TLDR</h4>
+            <p className="text-neutral-300 mt-2 tracking-tighter whitespace-pre-line">{frontmatter.tldr}</p>
+          </div>
+          <div className="mt-14">
+            <Post />
           </div>
         </div>
-        <Image
-          alt={frontmatter.title + ` ${frontmatter.description}`}
-          src={frontmatter.thumbnail}
-          width={1000}
-          height={1000}
-          className="h-[20rem] w-full rounded-lg object-cover shadow-md object-center"
-        />
-      </article>
-      <Post />
-    </div>
+        <TableOfContents headings={headings} />
+      </div>
+    </>
   );
 }
 
